195add9ff8c4e559ae59411f6f66aed8602e1f98
Matt Farrell <mfarrell@squareup.com>
2024-02-16 10:22:49 +1100

    cleanup spacing and comments

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
     if (details.tabId) {
-        // First, inject the CSS file
+
         chrome.scripting.insertCSS({
             target: { tabId: details.tabId },
-            files: ['content.css'] // Specify your CSS file here
+            files: ['content.css']
         }, () => {
             if (chrome.runtime.lastError) {
                 console.error('Error injecting CSS:', chrome.runtime.lastError.message);
             } else {
-                console.log("CSS injected successfully");
-                // Only attempt to inject the script after the CSS has been injected successfully
+
                 chrome.scripting.executeScript({
                     target: { tabId: details.tabId },
                     files: ['content.js']
@@ -17,8 +16,6 @@ chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
                     if (chrome.runtime.lastError) {
                         console.error('Error injecting script:', chrome.runtime.lastError.message);
                     } else {
-                        console.log("Script injected successfully");
-                        // Ensure this part runs after the successful injection of content3.js
                         chrome.tabs.sendMessage(details.tabId, { trigger: true });
                     }
                 });
--- content.js
+++ content.js
@@ -1,8 +1,5 @@
 if (typeof window.contentScriptInjected === 'undefined') {
     window.contentScriptInjected = true;
-    console.log("content3.js loaded");
-
-    // Function to wait for an element to be available within the DOM
     const waitForElement = (selector, parent = document, all = false) => {
         return new Promise((resolve, reject) => {
             const startTime = Date.now();
@@ -25,14 +22,12 @@ if (typeof window.contentScriptInjected === 'undefined') {
     };
 
     const addPinnedItemsDiv = async () => {
-        console.log('1. Adding pinned items div');
         const setupComponent = await waitForElement('#setupComponent > div.slds-page-header.branding-setup.onesetupSetupHeader > div > div.bBody');
         if (!setupComponent) throw new Error('setupComponent not found.');
 
         const existingDiv = setupComponent.querySelector('#pinned-items-container');
         if (existingDiv) {
             existingDiv.remove();
-            console.log('Removed existing Pinned items div.');
         }
 
         const containerDiv = document.createElement('div');
@@ -61,26 +56,17 @@ if (typeof window.contentScriptInjected === 'undefined') {
         containerDiv.appendChild(headerContainer);
         containerDiv.appendChild(pinnedItemsDiv);
         setupComponent.appendChild(containerDiv);
-
-        console.log('2. Pinned items div added.');
     };
 
-
-    // Wait for the table to have more than one row
     const waitForTablePopulation = async (table) => {
-        console.log('3. Waiting for table population');
         let rows = table.querySelectorAll('tbody tr');
-        while (rows.length <= 1) { // Check if the number of rows is 1 or less
+        while (rows.length <= 1) {
             await new Promise(resolve => setTimeout(resolve, 100));
-            // Re-query the rows inside the loop to get the updated count
             rows = table.querySelectorAll('tbody tr');
         }
-        console.log('4. Table populated');
     }
 
-    // Add "Pinned" column header if not already present
     const modifyTableHeader = async (table) => {
-        console.log('5. Modifying table header');
         const headerRow = table.querySelector('thead tr');
         if (!headerRow.querySelector('th.pinned-header')) {
             const pinnedHeader = document.createElement('th');
@@ -88,13 +74,9 @@ if (typeof window.contentScriptInjected === 'undefined') {
             pinnedHeader.classList.add('pinned-header');
             headerRow.appendChild(pinnedHeader);
         }
-        console.log('6. Table header modified');
     };
 
-    // Add "Pin" button to each row
     const modifyRows = async (table, pinnedItems) => {
-        console.log('6. Modifying Rows');
-
         const rows = await waitForElement('tbody tr', table, all = true);
         rows.forEach((row) => {
             if (!row.querySelector('td.pin-button-container')) {
@@ -102,7 +84,7 @@ if (typeof window.contentScriptInjected === 'undefined') {
                 let uniqueId = row.querySelector('td').textContent;
 
                 let td = document.createElement('td');
-                td.className = 'pin-button-container'; // This helps in identifying if a button has already been added
+                td.className = 'pin-button-container';
 
                 let button = document.createElement('button');
                 button.classList.add('pin-button');
@@ -114,7 +96,6 @@ if (typeof window.contentScriptInjected === 'undefined') {
                 row.appendChild(td);
             }
         });
-        console.log('7. Table modified');
     };
 
     const togglePin = async (uniqueId, url, button) => {
@@ -126,7 +107,6 @@ if (typeof window.contentScriptInjected === 'undefined') {
             pinnedItems[uniqueId] = url;
             button.classList.add('pinned');
         }
-
         localStorage.setItem('pinnedItems', JSON.stringify(pinnedItems));
         await updatePinnedItemsDiv();
     }
@@ -141,24 +121,21 @@ if (typeof window.contentScriptInjected === 'undefined') {
                 let button = document.createElement('button');
                 button.className = 'pinned-item-button';
                 button.textContent = key;
-                button.onclick = () => window.location.href = pinnedItems[key]; // Redirect to the URL on click
+                button.onclick = () => window.location.href = pinnedItems[key];
                 pinnedItemsDiv.appendChild(button);
             }
         }
     }
 
     chrome.runtime.onMessage.addListener(
-        async (request, sender, sendResponse) => {
+        async () => {
             try {
                 // const table = await waitForElement('#setupComponent div.setupcontent table');
                 const table = await waitForElement('#setupComponent > div.setupcontent > div > div.scroller.uiScroller.scroller-wrapper.scroll-bidirectional.native > div > table');
-                // await addPinnedItemsDiv();
                 if (table) {
-
                     let pinnedItems = JSON.parse(localStorage.getItem('pinnedItems') || '{}');
 
                     const applyModifications = async () => {
-                        console.log("Applying modifications to the table");
                         try {
                             await waitForTablePopulation(table);
                             await modifyTableHeader(table);
@@ -180,8 +157,6 @@ if (typeof window.contentScriptInjected === 'undefined') {
                     observer.observe(table, { childList: true, subtree: true });
 
                     await applyModifications();
-
-                    console.log('Initialization complete');
                 }
             } catch (error) {
                 console.error('Initialization failed', error);
